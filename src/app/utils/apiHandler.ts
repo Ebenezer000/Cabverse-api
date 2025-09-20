cbe209a31b3910a970d0a24f7f5e7b1a775cadd4
Ebenezer Akpas <akpasebenezer0@gmail.com>
2025-09-20 22:35:05 +0100

    Update APi Handler

--- src/app/utils/apiHandler.ts
+++ src/app/utils/apiHandler.ts
@@ -28,7 +28,7 @@ function getCorsHeaders(origin?: string) {
     "Access-Control-Allow-Origin": isAllowedOrigin ? origin : "*", // Allow all origins in development
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS, PATCH",
     "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With, Accept, Origin",
-    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Allow-Credentials": "false",
     "Access-Control-Max-Age": "86400", // Cache preflight for 24 hours
   };
 }
